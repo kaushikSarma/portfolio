7ac0c2070c5005791adc87323fe5aa7d89f4088f
Sarma <kaushik.sarma@accenture.com>
2019-01-24 22:34:50 +0530

    Updates

---
+++ precache-manifest.f47e46fd984f6b9fa2c455f156322946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "f40cd42c6a5a5ef7f561",
+    "url": "/portfolio/static/js/main.f40cd42c.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/portfolio/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f40cd42c6a5a5ef7f561",
+    "url": "/portfolio/static/css/main.7f91b06e.chunk.css"
+  },
+  {
+    "revision": "49caa7dfd9c25f1ce3e848173494f3ea",
+    "url": "/portfolio/index.html"
+  }
+];
