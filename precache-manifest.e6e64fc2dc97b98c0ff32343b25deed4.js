5d2e2b960b2028b5c429402d27d2dc14329c15d8
Sarma <kaushik.sarma@accenture.com>
2019-01-25 13:10:47 +0530

    Updates

---
+++ precache-manifest.e6e64fc2dc97b98c0ff32343b25deed4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "53a6ee19831d798433d3",
+    "url": "/portfolio/static/js/main.53a6ee19.chunk.js"
+  },
+  {
+    "revision": "142dd784c4b55efa9406",
+    "url": "/portfolio/static/js/1.142dd784.chunk.js"
+  },
+  {
+    "revision": "53a6ee19831d798433d3",
+    "url": "/portfolio/static/css/main.b8e4be7d.chunk.css"
+  },
+  {
+    "revision": "1f4825364349f68b3d9519989c91c2f8",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.557d5acde91854ccec79aeaa5e363dd8.js"
+  "/portfolio/precache-manifest.e6e64fc2dc97b98c0ff32343b25deed4.js"
 );
 
 workbox.clientsClaim();
